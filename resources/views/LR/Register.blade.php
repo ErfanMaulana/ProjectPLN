c89e98e6bdaefb9f484d2407917a9f8ad4619572
kaffah <Silmikaffahkaffah25@gmail.com>
2025-02-20 08:36:08 +0700

    Test

--- resources/views/LR/Register.blade.php
+++ resources/views/LR/Register.blade.php
@@ -3,10 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Register</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    <h1>Hallo world</h1>
+    <!-- Form Register -->
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6 form-container">
+                <h3 class="text-center">Register</h3>
+                <form action="{{ route('register') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                    </div>
+                    <p class="text-center">I have an account? <a href="{{ route('login') }}">Login</a></p>
+
+                    <button type="submit" class="btn btn-primary"> Register</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </body>
 </html>
