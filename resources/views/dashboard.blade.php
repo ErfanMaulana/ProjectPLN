5d06ee712bf44e40d3e31ec8627e363f2feb3310
RPL24 <rifkinauvaldzaki08@gmail.com>
2025-02-25 13:18:42 +0700

    hello

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
     <h1>Welcome to the Dashboard!</h1>
     <form action="{{ route('logout') }}" method="POST">
         @csrf
-        <button type="submit">Logout</button>
+        <button type="submit">Keluar</button>
     </form>
 </body>
 </html>
